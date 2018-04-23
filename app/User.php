943d4de8de0c3c6eb69fe9eebb6ffef4c0aa3bde
rachel <hai30@163.com>
2018-04-23 22:57:38 +0800

    first commit

---
+++ app/User.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use App\Mail\ResetPassword;
+use App\Mail\VerifyMail;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Mail;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'avarta', 'confirmation_token'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function sendPasswordResetNotification($token)
+    {
+        $data = [
+            'url' => route('password.reset', [
+                'token' => $token,
+            ]),
+        ];
+
+        Mail::to($this->email)->queue(new ResetPassword($data));
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Mail\TestMail;
+use Illuminate\Support\Facades\Mail;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/email/verify/{token}', 'EmailController@verify')->name('email.verify');
+
+
+
+
